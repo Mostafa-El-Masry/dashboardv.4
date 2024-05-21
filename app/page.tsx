970029b42c27afe4465d28ce770138313aac4acd
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-05-21 21:15:38 +0200

    text adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="flex h-screen bg-stone-500 capitalize">
+    <div className="flex h-screen bg-stone-500 capitalize text-lg">
     <div className='flex flex-col items-center gap-10 m-36'>
       <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 p-4">A year (Vication) goals calender</div>
-    <div className='flex flex-row gap-6 '>
+    <div className='flex flex-row gap-6 text-sm'>
       <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 p-4">Work Any Thing</div>
       <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 p-4">Next JS Mastering (dashboard)</div>
       <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500 p-4">Stable living situation</div>
