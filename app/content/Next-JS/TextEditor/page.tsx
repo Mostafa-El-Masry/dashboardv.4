775990b663e1f7c600b3d8f696bd1cca108ffcfd
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2024-06-02 02:47:52 +0300

    Creating a page for text editor

---
+++ app/content/Next-JS/TextEditor/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TextEditor = () => {
+  return (
+    <div>
+      <h1>Text Editor</h1>
+      <h2>Visual Studio Code</h2>
+      <h3>Extensions</h3>
+      <ul>
+        <li>ES7+ React/Redux/React-Native snippets</li>
+        <li>HTML Entity Converter</li>
+        <li>Live Server</li>
+        <li>Prettier - Code formatter</li>
+        <li>Tailwind CSS IntelliSense</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TextEditor;
--- app/content/Next-JS/page.tsx
+++ app/content/Next-JS/page.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 
 const NextJs = () => {
   return (
-    <div>Next JS Mastering (dashboard)<br />
-      <Link href="/">Home</Link><br />
+    <div>Next JS Mastering (dashboard)
+      <Link href="/">Home</Link>
       <Link href="/content/Next-JS/HTML">HTML</Link>
+      <Link href="/content/Next-JS/TextEditor">Text Editor</Link>
     </div>
 
   )
